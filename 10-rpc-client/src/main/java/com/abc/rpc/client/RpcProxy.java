603366472634d9ca9c09a94c75df745a5aa19f1e
656285856@qq.com <yu1687828380>
2020-01-14 09:38:16 +0800

    rpc

---
+++ 10-rpc-client/src/main/java/com/abc/rpc/client/RpcProxy.java
@@ -0,0 +1,75 @@
+package com.abc.rpc.client;
+
+import com.abc.rpc.bean.InvokeMessage;
+import com.abc.rpc.service.SomeService;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class RpcProxy {
+    // 泛型方法
+    public static <T> T create(Class<?> clazz) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(),
+                new Class[]{clazz},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        // 若调用的是Object中的方法，则直接进行本地调用
+                        if(Object.class.equals(method.getDeclaringClass())) {
+                            // 本地调用
+                            return method.invoke(this, args);
+                        }
+                        // 进行远程调用
+                        return rpcInvoke(clazz, method, args);
+                    }
+                });
+    }
+
+    private static Object rpcInvoke(Class<?> clazz, Method method, Object[] args) throws InterruptedException {
+        RpcClientHandler handler = new RpcClientHandler();
+        NioEventLoopGroup loopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(loopGroup)
+                    .channel(NioSocketChannel.class)
+                    // Nagle算法
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new ObjectEncoder());
+                            pipeline.addLast(new ObjectDecoder(Integer.MAX_VALUE,
+                                    ClassResolvers.cacheDisabled(null)));
+                            pipeline.addLast(handler);
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect("localhost", 8888).sync();
+
+            // 创建并初始化调用信息
+            InvokeMessage message = new InvokeMessage();
+            message.setClassName(clazz.getName());
+            message.setMethodName(method.getName());
+            message.setParamTypes(method.getParameterTypes());
+            message.setParamValues(args);
+            // 将调用信息发送给Server
+            future.channel().writeAndFlush(message).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            loopGroup.shutdownGracefully();
+        }
+        return handler.getResult();
+    }
+}
